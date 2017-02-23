eec2f20b67035cfd73e44b543f94cc9499a20bb1
Kris Moore <kris@ixsystems.com>
2017-02-22 20:28:03 -0500

    Restore some merge loss of LibreSSL-devel patch

--- net/qt5-network/files/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
+++ net/qt5-network/files/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
@@ -1,8 +1,15 @@
-* Boilerplate for SSL_CTX_set1_curves/groups() used in qsslcontext_openssl.cpp
-*
---- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2016-09-16 05:49:42 UTC
-+++ src/network/ssl/qsslsocket_openssl_symbols_p.h
-@@ -481,6 +481,8 @@ void q_EC_KEY_free(EC_KEY *ecdh);
+--- src/network/ssl/qsslsocket_openssl_symbols_p.h	2017-02-22 23:44:03 UTC
++++ src/network/ssl/qsslsocket_openssl_symbols_p.h	
+@@ -74,6 +74,8 @@ QT_BEGIN_NAMESPACE
+ 
+ #define DUMMYARG
+ 
++#define SSL_CTRL_GET_SERVER_TMP_KEY 109
++
+ #if !defined QT_LINKED_OPENSSL
+ // **************** Shared declarations ******************
+ // ret func(arg)
+@@ -487,6 +489,8 @@ void q_EC_KEY_free(EC_KEY *ecdh);
  size_t q_EC_get_builtin_curves(EC_builtin_curve *r, size_t nitems);
  #if OPENSSL_VERSION_NUMBER >= 0x10002000L
  int q_EC_curve_nist2nid(const char *name);
@@ -10,4 +17,4 @@
 +int q_SSL_CTX_set1_groups(SSL_CTX *a, const int *b, size_t c);
  #endif // OPENSSL_VERSION_NUMBER >= 0x10002000L
  #endif // OPENSSL_NO_EC
- 
+ #if OPENSSL_VERSION_NUMBER >= 0x10002000L
